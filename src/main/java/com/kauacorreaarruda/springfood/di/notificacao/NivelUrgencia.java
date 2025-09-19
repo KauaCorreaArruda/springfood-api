03fdaf0584718fe58bf84e61d03ef94342319f06
kauacorreadev <kaua.correa.arruda@gmail.com>
2025-09-19 19:21:54 -0300

    Adding and configuring JPA

--- src/main/java/com/kauacorreaarruda/springfood/di/notificacao/NivelUrgencia.java
+++
@@ -1,7 +0,0 @@
-package com.kauacorreaarruda.springfood.di.notificacao;
-
-public enum NivelUrgencia {
-
-    URGENTE,
-    SEM_URGENCIA
-}
--- src/main/java/com/kauacorreaarruda/springfood/di/notificacao/Notificador.java
+++
@@ -1,7 +0,0 @@
-package com.kauacorreaarruda.springfood.di.notificacao;
-
-import com.kauacorreaarruda.springfood.di.modelo.Cliente;
-
-public interface Notificador {
-    void notificar(Cliente cliente, String mensagem);
-}
