0cd3fff3829837d26d9b785ac8b859948a04f7fc
kauacorreadev <kaua.correa.0508@gmail.com>
2025-09-24 21:55:59 -0300

    Updating an object in the database

--- src/main/java/com/kauacorreaarruda/springfood/jpa/KitchenRegistration.java
+++ src/main/java/com/kauacorreaarruda/springfood/jpa/KitchenRegistration.java
@@ -24,7 +24,7 @@ public class KitchenRegistration {
     }
 
     @Transactional
-    public Kitchen add(Kitchen kitchen) {
+    public Kitchen save(Kitchen kitchen) {
         return manager.merge(kitchen);
     }
 }
---
+++ src/main/java/com/kauacorreaarruda/springfood/jpa/UpdateKitchenMain.java
@@ -0,0 +1,25 @@
+package com.kauacorreaarruda.springfood.jpa;
+
+import com.kauacorreaarruda.springfood.SpringfoodApiApplication;
+import com.kauacorreaarruda.springfood.domain.model.Kitchen;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+
+public class UpdateKitchenMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(SpringfoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        KitchenRegistration kitchenRegistration = applicationContext.getBean(KitchenRegistration.class);
+
+        Kitchen kitchen = new Kitchen();
+        kitchen.setId(1L);
+        kitchen.setName("Brazilian");
+
+        kitchenRegistration.save(kitchen);
+    }
+}
