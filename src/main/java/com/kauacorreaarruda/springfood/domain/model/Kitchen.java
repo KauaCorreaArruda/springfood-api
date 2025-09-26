5e02cb8815e9d322c229c6ea313c12359fb873a0
kauacorreadev <kaua.correa.0508@gmail.com>
2025-09-26 15:37:53 -0300

    Nullable property in Column and joinColumn annotation

--- src/main/java/com/kauacorreaarruda/springfood/domain/model/Kitchen.java
+++ src/main/java/com/kauacorreaarruda/springfood/domain/model/Kitchen.java
@@ -14,5 +14,6 @@ public class Kitchen {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 }
--- src/main/java/com/kauacorreaarruda/springfood/domain/model/Restaurant.java
+++ src/main/java/com/kauacorreaarruda/springfood/domain/model/Restaurant.java
@@ -16,11 +16,13 @@ public class Restaurant {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
-    
+
+    @Column(name = "freight_rate", nullable = false)
     private BigDecimal freightRate;
 
     @ManyToOne
-    @JoinColumn(name = "kitchen_id")
+    @JoinColumn(name = "kitchen_id", nullable = false)
     private Kitchen kitchen;
 }
