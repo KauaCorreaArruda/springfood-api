bb761c01397a7b6dbc42d938b4b117a778bcbc51
kauacorreadev <kaua.correa.0508@gmail.com>
2025-10-08 12:17:30 -0300

    Content negotiation to return JSON or XML

--- src/main/java/com/kauacorreaarruda/springfood/api/resource/KitchenResource.java
+++ src/main/java/com/kauacorreaarruda/springfood/api/resource/KitchenResource.java
@@ -2,10 +2,12 @@ package com.kauacorreaarruda.springfood.api.resource;
 
 import com.kauacorreaarruda.springfood.domain.model.Kitchen;
 import com.kauacorreaarruda.springfood.domain.repository.KitchenRepository;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
 import java.util.List;
 
 @RestController
@@ -18,8 +20,16 @@ public class KitchenResource {
         this.kitchenRepository = kitchenRepository;
     }
 
-    @GetMapping
-    public List<Kitchen> findAll() {
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Kitchen> findAll1() {
+        System.out.println("findAll1");
+        return  kitchenRepository.findAll();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public List<Kitchen> findAll2() {
+        System.out.println("findAll2");
         return  kitchenRepository.findAll();
     }
 }
